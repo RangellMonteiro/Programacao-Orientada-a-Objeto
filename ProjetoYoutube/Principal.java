b4f611f3102d3cd97e93a2925aee56abed5fb36c
RangellMonteiro <rangell_monteiro@hotmail.com>
2024-08-17 02:34:48 -0300

    Projeto final em java(parte 1)

---
+++ ProjetoYoutube/Principal.java
@@ -0,0 +1,12 @@
+package ProjetoYoutube;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Gafanhoto g = new Gafanhoto("Mario", 22, "Masculino", 40, "Mario123");
+
+        System.out.println(g.toString());
+
+    }
+
+}
---
+++ ProjetoYoutube/Video.java
@@ -0,0 +1,106 @@
+package ProjetoYoutube;
+
+public class Video implements AcoesVideo {
+    private String titulo;
+    private int avaliacao;
+    private int views;
+    private int curtidas;
+    private boolean reproduzindo;
+
+    public Video(String titulo, int avaliacao, int views, int curtidas, boolean reproduzindo) {
+        this.titulo = titulo;
+        this.avaliacao = 1;
+        this.views = 0;
+        this.curtidas = 0;
+        this.reproduzindo = false;
+    }
+
+    @Override
+    public void play() {
+        // TODO Auto-generated method stub
+        this.reproduzindo = true;
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        this.reproduzindo = false;
+    }
+
+    @Override
+    public void like() {
+        // TODO Auto-generated method stub
+        this.curtidas++;
+    }
+
+    /**
+     * @return String return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return int return the avaliacao
+     */
+    public int getAvaliacao() {
+        return avaliacao;
+    }
+
+    /**
+     * @param avaliacao the avaliacao to set
+     */
+    public void setAvaliacao(int avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    /**
+     * @return int return the views
+     */
+    public int getViews() {
+        return views;
+    }
+
+    /**
+     * @param views the views to set
+     */
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    /**
+     * @return int return the curtidas
+     */
+    public int getCurtidas() {
+        return curtidas;
+    }
+
+    /**
+     * @param curtidas the curtidas to set
+     */
+    public void setCurtidas(int curtidas) {
+        this.curtidas = curtidas;
+    }
+
+    /**
+     * @return boolean return the reproduzindo
+     */
+    public boolean getReproduzindo() {
+        return reproduzindo;
+    }
+
+    /**
+     * @param reproduzindo the reproduzindo to set
+     */
+    public void setReproduzindo(boolean reproduzindo) {
+        this.reproduzindo = reproduzindo;
+    }
+
+}
